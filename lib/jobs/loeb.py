6e2bc16f9439f4a1e39b678e6361c08cdcf70bb2
grouchomonkey <jason.stewart@coloradocollege.edu>
2015-08-14 11:40:56 -0600

    Update loeb.py

--- lib/jobs/loeb.py
+++ lib/jobs/loeb.py
@@ -9,3 +9,29 @@ class LoebClassicalLibrary(MARCModifier):
     def processRecord(self,marc_record):
         ''' Method should be overriddden by derived classes.'''
         pass
+    
+    
+    def validate710(self,marc_record):
+        """
+        :param marc_record: MARC record, required
+        """
+        self.__remove_field__(marc_record=marc_record,
+                              tag='710')
+        field730 = Field(tag='710',
+                         indicators=['2',' '].
+                         subfields=['a','Loeb Classical Library'])
+    marc_record.add_field(field730)
+    return marc_record
+    
+    
+    def validate730(self,marc_record):
+        """
+        :param marc_record: MARC record, required
+        """
+        self.__remove_field__(marc_record=marc_record,
+                              tag='730')
+        field730 = Field(tag='730',
+                         indicators=['0',' '].
+                         subfields=['a','Loeb Classical Library'])
+    marc_record.add_field(field730)
+    return marc_record
